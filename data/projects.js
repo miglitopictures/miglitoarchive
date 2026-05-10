ceb93274f088b1abaab941a8f3e6eca1bf2c947b
miglitopictures <miguelmddesign@gmail.com>
2026-05-10 12:15:58 -0300

    content: filling out projects

--- data/projects.js
+++ data/projects.js
@@ -1,3 +1,5 @@
+export const author = "Miguel Duarte";
+
 export const works = {
     "histerica": {
         title:"Histérica",
@@ -5,24 +7,46 @@ export const works = {
         studio: "NOO",
         client: "Plau",
         categories: ["design", "motion"],
-        thumbnail: "https://mir-s3-cdn-cf.behance.net/project_modules/fs/750683240721445.6944516041736.png",
-        preview_thumb: "https://mir-s3-cdn-cf.behance.net/project_modules/fs/750683240721445.6944516041736.png",
-        preview_alt: "Newspaper cut-out of a woman subtly smilling with the written word 'Histéca', in vivid lime green, centered in the frame.",
-        // preview: "",
-        // role: [art_direction, graphic_design, creative_direction, animation],
 
+        preview_thumb: "https://mir-s3-cdn-cf.behance.net/project_modules/fs/750683240721445.6944516041736.png",
+        preview_video: "https://res.cloudinary.com/dobmgp6ug/video/upload/f_auto,q_auto,w_800/v1770769166/KIMURA_PREVIEW_hcy3wk.mp4",
+        preview_alt: "Newspaper cut-out of a woman subtly smilling with the written word 'Histéca', in vivid lime green, centered in the frame.",
+        
+        credits:{
+            direction: ["Plau", "Ana Paula Ferraz"],
+            type_design: ["Plau", "Ana Paula Ferraz"],
+            manifesto: ["Plau", "Ana Paula Ferraz"],
+            creative_direction: [author, "Louise Favilla"],
+            art_direction: [author],
+            design_direction:["Deco Moreira"],
+            design: [author, "Louise Favilla", "Nathália Santos"],
+            animation_direction: ["Marcio Sal"],
+            animation: [author],
+            copywriting: ["Camila Inácio", "Gabriel Pardal"],
+            project_management: ["Fábio Neves"],
+            account_management: ["Ana Luisa Rodrigues"],
+            operations_direction: ["Kathlyn Giaccomini"]
+        }
     },
+
     "lembrete": {
         title:"Lembrete",
         year: 2023,
         studio: "NOO",
         client: "NOO",
         categories: ["3d", "animation", "short film"],
-        preview_thumb: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200_webp/1bcaab159061067.63977d30ed321.png",
-        preview_alt: "3D clay render of a runner about to cross a lonely finish line. The image is a wide angle, diagonaly simmetric, overhead shot of a hot minimalistic desetd. The runner, frozen mid-run, wears a red headband, white shirt and green shorts.",
-        // preview: "",
-        // role: [art_direction, graphic_design, creative_direction, animation],
 
+        preview_thumb: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200_webp/1bcaab159061067.63977d30ed321.png",
+        preview_video: "https://res.cloudinary.com/dr5dbkh91/video/upload/f_auto,q_auto,w_800/v1770302765/projects/tap-to-pay-zoop/y1droueioptnwkagby0j.mp4",
+        preview_alt: "3D clay render of a runner about to cross a lonely finish line. The image is a wide angle, diagonaly simmetric, overhead shot of a hot minimalistic desetd. The runner, frozen mid-run, wears a red headband, white shirt and green shorts.",
+    
+        credits:{
+            creative_direction: ["Judd Buchanan", author],
+            storyboarding: [author],
+            character_design: [author],
+            modeling: ["Judd Buchanan"],
+            character_animation: ["Judd Buchanan"],
+        }
     },
 
     "stonecamp23": {
@@ -31,11 +55,10 @@ export const works = {
         studio: "NOO",
         client: "Stone",
         categories: ["3d", "motion", "graphic design"],
-        preview_thumb: "https://mir-s3-cdn-cf.behance.net/project_modules/1400/6a1797188504087.65a83bcf5f2a1.png",
-        preview_alt: "3D clay render of a runner about to cross a lonely finish line. The image is a wide angle, diagonaly simmetric, overhead shot of a hot minimalistic desetd. The runner, frozen mid-run, wears a red headband, white shirt and green shorts.",
-        // preview: "",
-        // role: [art_direction, graphic_design, creative_direction, animation],
 
+        preview_thumb: "https://mir-s3-cdn-cf.behance.net/project_modules/1400/6a1797188504087.65a83bcf5f2a1.png",
+        preview_video: "https://res.cloudinary.com/dobmgp6ug/video/upload/f_auto,q_auto,w_800/v1770769166/KIMURA_PREVIEW_hcy3wk.mp4",
+        preview_alt: "3D clay render of a runner about to cross a lonely finish line. The image is a wide angle, diagonaly simmetric, overhead shot of a hot minimalistic desetd. The runner, frozen mid-run, wears a red headband, white shirt and green shorts.",
     },
 
     "tap-to-pay": {
@@ -44,12 +67,23 @@ export const works = {
         studio: "NOO",
         client: "Zoop",
         categories: ["3d", "motion", "graphic design"],
+
         preview_thumb: "https://res.cloudinary.com/dr5dbkh91/video/upload/f_auto,q_auto,w_800,so_0/v1770302765/projects/tap-to-pay-zoop/y1droueioptnwkagby0j.jpg",
         preview_video: "https://res.cloudinary.com/dr5dbkh91/video/upload/f_auto,q_auto,w_800/v1770302765/projects/tap-to-pay-zoop/y1droueioptnwkagby0j.mp4",
         preview_alt: "3D clay render of a runner about to cross a lonely finish line. The image is a wide angle, diagonaly simmetric, overhead shot of a hot minimalistic desetd. The runner, frozen mid-run, wears a red headband, white shirt and green shorts.",
-        // preview: "",
-        // role: [art_direction, graphic_design, creative_direction, animation],
 
+        credits:{
+            conception: ["Grupo Sal"],
+            copywriting: ["Grupo Sal"],
+            direction: ["Grupo Sal"],
+            production: ["Grupo Sal"],
+            animation: ["Fernando Villela", author, "Judd Buchanan"],
+            art_direction:["Deco Moreira", "Lucas Noel"],
+            design:["Lucas Noel", "Deco Moreira", "Fernando Villela", author],
+            project_management: ["Fábio Neves"],
+            account_management: ["Ana Luisa Rodrigues"],
+            post_production: ["Marcio Sal"]
+        }
     },
 
     "kimura-sans": {
@@ -58,12 +92,21 @@ export const works = {
         studio: "NOO",
         client: "Plau",
         categories: ["3d", "motion", "graphic design"],
+
         preview_thumb: "https://res.cloudinary.com/dobmgp6ug/video/upload/f_auto,q_auto,w_800,so_0/v1770769166/KIMURA_PREVIEW_hcy3wk.jpg",
         preview_video: "https://res.cloudinary.com/dobmgp6ug/video/upload/f_auto,q_auto,w_800/v1770769166/KIMURA_PREVIEW_hcy3wk.mp4",
         preview_alt: "3D clay render of a runner about to cross a lonely finish line. The image is a wide angle, diagonaly simmetric, overhead shot of a hot minimalistic desetd. The runner, frozen mid-run, wears a red headband, white shirt and green shorts.",
-        // preview: "",
-        // role: [art_direction, graphic_design, creative_direction, animation],
 
+        credits:{
+            account_management: ["Antonio Carvalho"],
+            executive_production: ["Kathlyn Giaccomini"],
+            art_direction:["Beatriz de Andrade"],
+            direction: ["Marcio Sal"],
+            copywriting: ["Gabriel Pardal", "Giovanna Ferrari"],
+            design:["Louise Favilla", "Beatriz de Andrade", "Fernando Villela", author],
+            animation: ["Fernando Villela", author],
+            project_management: ["Consoante Soma de Sons"]
+        }
     },
 }
 
