8c52581a636852d3abb6a95fc0a30331d073928f
miglitopictures <miguelmddesign@gmail.com>
2026-07-21 19:28:34 -0300

    setup unobserve

--- src/main.js
+++ src/main.js
@@ -33,7 +33,7 @@ const observer = new IntersectionObserver((entries, observer) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
       entry.target.classList.add('is-visible');
-    //   observer.unobserve(entry.target);
+      observer.unobserve(entry.target);
     } else {
         entry.target.classList.remove('is-visible');
     }
